4c702a4a04b71cf12ecc80091ff7b9c79a1245e7
Christos Orestis <orestisc@thomas.edu>
2019-09-18 14:41:52 -0400

    added a number of #includes and usings, changed add to push_back, modifed both for loops to work as intended, switched winner and endl in the last cout.

--- CS245-F2019-Lab2/CS245-F2019-Lab2.cpp
+++ CS245-F2019-Lab2/CS245-F2019-Lab2.cpp
@@ -15,45 +15,55 @@
 
  // Preprocessor directives
 #include <random>
+#include <string>
+#include <vector>
+#include <iostream>
 
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
 
 int main()
 {
 	// Create a vector of strings; add items using a list initializer
-	vector<String> movies = "Holmes & Watson", "Mega Shark vs. Crocosaurus", "C-SPAN: The Movie", "That's My Boy";
-
+	vector<string> movies { "Holmes & Watson", "Mega Shark vs. Crocosaurus", "C-SPAN: The Movie", "That's My Boy" };
+	
 	// Add a 5th string to the end of the vector
-	movies.add("Titanic 2: The Quickening");
+	movies.push_back("Titanic 2: The Quickening");
 
 	// Announce the nominees
-	cout << "And the nominees for Best Picture (cough) are..." >> endl;
+	cout << "And the nominees for Best Picture (cough) are..." << endl;
 
 	// Loop through the vector; print each vector element to console.
-	for (i = 0; i < movies.Length; i++)
+	int i;
+	int z = movies.size();
+	for (i = 0; i < z; i++)
 	{
-		cout << movies.get(i) << endl;
+		cout << movies[i] << endl;
 	}
 
 	// Set up a random number generator seeder
 	std::random_device seed;
 
 	// Create a random integer generator; chooses random ints from 1 to 5 (inclusive)
-	std::uniform_int_distribution<int> rand_dist(1, 6);
+	std::uniform_int_distribution<int> rand_dist(1, 5);
 
 	// Get the next random integer
 	int pick_a_number = rand_dist(seed);
 
 	// Get the item from the vector at the randomly-selected index
-	String winner = movies.get[pick_a_number];
+	string winner = movies[pick_a_number];
 
 	// Convert each charcter of the 'winner' string to UPPERCASE
-	for (char c : winner)
+	for (auto& c : winner)
 	{
-		c = c.toUpper();
+		c = toupper(c);
 	}
 
 	// Print the UPPERCASE string
-	cout << endl << "And the winner is..." << winner;
+	cout << "And the winner is... " << winner << endl;
 
 	return 0;
 }
